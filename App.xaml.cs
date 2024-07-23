eab71309294b658e9300dcf967c784e0e2f97b6d
Montrell <mjubilee@leaptodigital.com>
2024-07-22 20:58:58 -0400

    first commit

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace DemoCalculator;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,121 @@
+﻿namespace DemoCalculator;
+
+public partial class MainPage : ContentPage
+{
+    string currentEntry = "";
+    int currentState = 1;
+    string mathOperator;
+    double firstNumber, secondNumber;
+    string decimalFormat = "N0";
+    
+    public MainPage()
+    {
+        InitializeComponent();
+        OnClear(this, null);
+    }
+
+    void OnSelectNumber(object sender, EventArgs e)
+    {
+        Button button = (Button)sender;
+        string pressed = button.Text;
+
+        currentEntry += pressed;
+
+        if ((ResultText.Text == "0" && pressed == "0")
+            || (currentEntry.Length <= 1 && pressed != "0")
+            || currentState < 0)
+        {
+            ResultText.Text = "";
+            if (currentState < 0)
+                currentState *= -1;
+        }
+
+        if (pressed == "." && decimalFormat != "N2")
+        {
+            decimalFormat = "N2";
+        }
+
+        ResultText.Text += pressed;
+    }
+
+    void OnSelectOperator(object sender, EventArgs e)
+    {
+        LockNumberValue(ResultText.Text);
+
+        currentState = -2;
+        Button button = (Button)sender;
+        string pressed = button.Text;
+        mathOperator = pressed;            
+    }
+
+    private void LockNumberValue(string text)
+    {
+        double number;
+        if (double.TryParse(text, out number))
+        {
+            if (currentState == 1)
+            {
+                firstNumber = number;
+            }
+            else
+            {
+                secondNumber = number;
+            }
+
+            currentEntry = string.Empty;
+        }
+    }
+
+    void OnClear(object sender, EventArgs e)
+    {
+        firstNumber = 0;
+        secondNumber = 0;
+        currentState = 1;
+        decimalFormat = "N0";
+        ResultText.Text = "0";
+        currentEntry = string.Empty;
+    }
+
+    void OnCalculate(object sender, EventArgs e)
+    {
+        if (currentState == 2)
+        {
+            if (secondNumber == 0)
+                LockNumberValue(ResultText.Text);
+
+            double result = DemoCalculator.Calculate(firstNumber, secondNumber, mathOperator);
+
+            CurrentCalculation.Text = $"{firstNumber} {mathOperator} {secondNumber}";
+
+            ResultText.Text = result.ToString(decimalFormat);
+            firstNumber = result;
+            secondNumber = 0;
+            currentState = -1;
+            currentEntry = string.Empty;
+        }
+    }    
+
+    void OnNegative(object sender, EventArgs e)
+    {
+        if (currentState == 1)
+        {
+            secondNumber = -1;
+            mathOperator = "×";
+            currentState = 2;
+            OnCalculate(this, null);
+        }
+    }
+
+    void OnPercentage(object sender, EventArgs e)
+    {
+        if (currentState == 1)
+        {
+            LockNumberValue(ResultText.Text);
+            decimalFormat = "N2";
+            secondNumber = 0.01;
+            mathOperator = "×";
+            currentState = 2;
+            OnCalculate(this, null);
+        }
+    }
+}
